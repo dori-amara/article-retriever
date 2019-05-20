6e93082de165903cf3a847c950e6dc706cc47d89
Dori Amara <dorifamara@gmail.com>
2019-05-19 21:24:10 -0400

    Change date_published to DateTimeField

--- retriever/models.py
+++ retriever/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 class Article(models.Model):
     date_retrieved = models.DateTimeField(default=timezone.now)
     source_id = models.CharField(max_length=256)
-    date_published = models.CharField(max_length=20)
+    date_published = models.DateTimeField()
     author = models.CharField(max_length=256)
     title = models.CharField(max_length=256)
     url = models.CharField(max_length=512)
